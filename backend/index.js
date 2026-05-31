46c464a914062036482b38a05b0e39a74bc2cf66
RizwanAhmad <engrzain970@gmail.com>
2026-05-31 16:20:06 +0200

    update url in both sides

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chat-app-12-zsk9.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
