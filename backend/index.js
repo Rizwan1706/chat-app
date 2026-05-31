b4a8747610374590f5c83cb413a339eb2a6e37e5
RizwanAhmad <engrzain970@gmail.com>
2026-05-31 15:16:00 +0200

    error solved

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/message",messageRoute);
 app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-app.get("*", (_, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
  
